3f4d86038b3c8c72ab2e37a999c1f348a5ed0bbb
leonardojrr <leonardojrr1@gmail.com>
2023-04-10 20:47:30 -0400

    Adding the connect_rooms macro and changing the router macro

--- src/lib.rs
+++ src/lib.rs
@@ -7,38 +7,22 @@ pub mod room;
 mod room_builder;
 mod user;
 
-// #[macro_export]
-// macro_rules! run_server{
-//     ($port:expr, $($tokens:tt)+)=>{
-//         {
-//             let room_channels = HashMap::new();
-//             let mut socket = SocketListener::new($port, room_channels);
-
-//             let mut handlers_list = router!(socket, $($tokens)+);
-//             let handler = tokio::spawn(async move {socket.listen().await});
-
-//             handlers_list.push(handler);
-
-//             tokio::spawn(async move{
-//                 for handler in handlers_list{
-//                     let _ = handler.await;
-//                  }
-//             })
-//         }
-//     };
-// }
-
 #[macro_export]
-macro_rules! router {
-    ($($room: ident => [$($other_room: ident),+]),+) => {{
-        let mut rooms: HashMap<String, UnboundedSender<protocol::Room>> = HashMap::new();
-
-        $(rooms.insert($room.name.clone(), $room.sender.clone());)+
+macro_rules! connect_rooms {
+    ($($room: ident => [$($other_room: ident),+]),+) => {
         $(
             {
                 $($room.connect_room($other_room.clone()).await;)+
             }
         )+
+    };
+}
+
+#[macro_export]
+macro_rules! router {
+    ($($room: ident),+) => {{
+        let mut rooms: HashMap<String, UnboundedSender<protocol::Room>> = HashMap::new();
+        $(rooms.insert($room.name.clone(), $room.sender.clone());)+
 
         rooms
     }};
